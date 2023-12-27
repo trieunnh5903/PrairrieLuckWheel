58c78b87fd6681212ed18e46dbe9e19c7d32fdc4
trieunnh5903 <111626907+trieunnh5903@users.noreply.github.com>
2023-12-27 14:55:39 +0700

    .

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Alert, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import Animated, {
   Easing,
+  runOnJS,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
 
-const rates = [0, 0.2, 0.2, 0.6, 0, 0, 0, 0];
+const rates = [0.4, 0.2, 0.2, 0.6, 0, 0, 0, 0];
+
+const prizes = [
+  {id: 1, name: 'Giải nhất', angle: 0},
+  {id: 2, name: 'Giải 2', angle: 45},
+  {id: 3, name: 'Giải 3', angle: 90},
+  {id: 4, name: 'Giải 4', angle: 135},
+  {id: 5, name: 'Giải 5', angle: 180},
+  {id: 6, name: 'Giải 6', angle: 225},
+  {id: 7, name: 'Giải 7', angle: 270},
+  {id: 8, name: 'Giải 8', angle: 315},
+];
 const App = () => {
   const rotationValue = useSharedValue(0);
   const totalRate = rates.reduce((acc, rate) => acc + rate, 0);
@@ -27,18 +39,58 @@ const App = () => {
         break;
       }
     }
-
-    const degress = (360 / rates.length) * selectedSegment + 10;
-    console.log('degress', degress);
-
+    // Generate a random number between 10 and 40
+    const randomNum = Math.floor(Math.random() * 31 + 10);
+    const degressResult = (360 / rates.length) * selectedSegment;
+    const degressRotate = (360 / rates.length) * selectedSegment + randomNum;
+    console.log('degressResult', degressResult);
+    console.log('degressRotate', degressRotate);
+    const result = determinePrize(degressResult);
     rotationValue.value = 0;
-    rotationValue.value = withTiming(40 * 360 - degress, {
-      duration: 40 * 1000,
-      // easing: Easing.bezier(0.42, 0, 0.58, 1),
-      easing: Easing.out(Easing.quad),
-    });
+    rotationValue.value = withTiming(
+      10 * 360 - degressRotate,
+      {
+        duration: 10 * 1000,
+        // easing: Easing.bezier(0.42, 0, 0.58, 1),
+        easing: Easing.out(Easing.quad),
+      },
+      finished => {
+        if (finished) {
+          runOnJS(showNotification)(result);
+        }
+      },
+    );
   };
 
+  const showNotification = (message: string | undefined) => {
+    if (message) {
+      Alert.alert('Thông báo', `Bạn đã trúng ${message}`);
+    } else {
+      Alert.alert('Thông báo', 'Hệ thống đang gặp sự cố vui lòng thử lại sau}');
+    }
+  };
+  const determinePrize = (degress: number) => {
+    switch (degress) {
+      case 0:
+        return prizes[0].name;
+      case 45:
+        return prizes[1].name;
+      case 90:
+        return prizes[2].name;
+      case 135:
+        return prizes[3].name;
+      case 180:
+        return prizes[4].name;
+      case 225:
+        return prizes[5].name;
+      case 270:
+        return prizes[6].name;
+      case 315:
+        return prizes[7].name;
+      default:
+        break;
+    }
+  };
   const wheelAnimatedStyle = useAnimatedStyle(() => {
     return {
       transform: [
